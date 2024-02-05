d3fc1c7bce9e728f172e4d5b8bce3462f19ca2f2
ruhtar <arthur.amorim10@gmail.com>
2024-02-05 20:02:36 -0300

    pg em docker e connection string

--- RinhaDeBackend/Utils.cs
+++ RinhaDeBackend/Utils.cs
@@ -2,6 +2,6 @@
 {
     public static class Utils
     {
-        public const string ConnectionString = "Host=localhost;Port=5432;Database=rinhaTesteLocal;Username=postgres;Password=1234";
+        public const string ConnectionString = "Host=localhost;Port=5433;Database=rinha;Username=postgres;Password=123";
     }
 }

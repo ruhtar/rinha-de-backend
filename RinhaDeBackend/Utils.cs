b9a48133359c834a14a3e82ed43fdcfd356d6c72
ruhtar <arthur.amorim10@gmail.com>
2024-02-28 21:24:20 -0300

    fixes - wip

--- RinhaDeBackend/Utils.cs
+++ RinhaDeBackend/Utils.cs
@@ -2,6 +2,6 @@
 {
     public static class Utils
     {
-        public const string ConnectionString = "Host=host.docker.internal;Port=5433;Database=rinha;Username=postgres;Password=123;Pooling=true;Minimum Pool Size=30;Maximum Pool Size=30;Multiplexing=true;Timeout=15;Command Timeout=15;Cancellation Timeout=-1;No Reset On Close=true;Max Auto Prepare=20;Auto Prepare Min Usages=1";
+        public const string ConnectionString = "Host=localhost;Port=5433;Database=rinha;Username=postgres;Password=123;Pooling=true;Minimum Pool Size=30;Maximum Pool Size=30;Multiplexing=true;Timeout=15;Command Timeout=15;Cancellation Timeout=-1;No Reset On Close=true;Max Auto Prepare=20;Auto Prepare Min Usages=1";
     }
 }
